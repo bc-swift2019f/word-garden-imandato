b56bea8cfcff80e0852b20a2cc538636147e9ea5
Ivelisse Mandato <mandatoi@bc.edu>
2019-09-15 20:24:01 -0400

    3.3 Completed

--- Word Garden/ViewController.swift
+++ Word Garden/ViewController.swift
@@ -19,8 +19,8 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("In viewDidLoad, is guessedLetterField the first responder?", guessedLetterField.isFirstResponder)
-        // Do any additional setup after loading the view.
+        guessLetterButton.isEnabled = false
+        playAgainButton.isHidden = true
     }
 
     func updateUIAfterGuess(){
@@ -29,7 +29,13 @@ class ViewController: UIViewController {
     }
     
     @IBAction func guessedLetterFieldChanged(_ sender: UITextField) {
-        print("Hey! The guessedLetterFieldChanged!!!")
+        if let letterGuessed = guessedLetterField.text?.last {
+            guessedLetterField.text = "\(letterGuessed)"
+            guessLetterButton.isEnabled = true
+        } else {
+            //disable the button if I don't have a single character in the guessedLetterField
+            guessLetterButton.isEnabled = false
+        }
     }
     
     @IBAction func doneKeyPressed(_ sender: UITextField) {
